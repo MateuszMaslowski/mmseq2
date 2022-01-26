d587ace133a26fc63c88b3b20958185c51db22d6
MateuszMaslowski <maslowskim255@gmail.com>
2022-01-26 19:15:33 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/MMSeq2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MMSeq2.dir/main.cpp.o"
+  "MMSeq2"
+  "MMSeq2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MMSeq2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
