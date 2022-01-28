854066446cb79b2a8a9d13a758fb43bf4ab51104
MateuszMaslowski <maslowskim255@gmail.com>
2022-01-28 01:17:22 +0100

    MVP

--- cmake-build-debug/CMakeFiles/MMSeq2.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/MMSeq2.dir/cmake_clean.cmake
@@ -1,5 +1,10 @@
 file(REMOVE_RECURSE
+  "CMakeFiles/MMSeq2.dir/blosum.cpp.o"
   "CMakeFiles/MMSeq2.dir/main.cpp.o"
+  "CMakeFiles/MMSeq2.dir/mmseq.cpp.o"
+  "CMakeFiles/MMSeq2.dir/mock_structures.cpp.o"
+  "CMakeFiles/MMSeq2.dir/qsequence.cpp.o"
+  "CMakeFiles/MMSeq2.dir/tsequence.cpp.o"
   "MMSeq2"
   "MMSeq2.pdb"
 )
